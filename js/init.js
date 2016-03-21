f7324368afe520a909c3e6e807d4e6b4ff2048ef
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-03-21 17:57:44 +0200

    beginning

---
+++ js/init.js
@@ -0,0 +1,14 @@
+(function($){
+  $(function(){
+
+  
+// $(window).resize(function() {
+//   var mainTitle = document.querySelector("img");
+//   		var mainTitleH2 = document.querySelector(".card-title h3");
+//   		mainTitleH2.setAttribute("fontize", mainTitle.height + "px");
+//   		// console.log(mainTitleH2.style.fontSize);
+//   		console.log(mainTitleH2);
+// });
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
