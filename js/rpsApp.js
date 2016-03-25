691655b5eca4fb43291d5eb50dad0fe46b32a697
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-03-26 00:04:01 +0200

    -

--- js/rpsApp.js
+++ js/rpsApp.js
@@ -1,8 +1,25 @@
 var rpsApp = angular.module('rpsApp', []);
+rpsApp.run(function($rootScope){
+
+});
 rpsApp.controller("rpsCtrl", function($scope){
 	$scope.user = {
 		male: 0,
 		age: 12
 	};
 	console.log("controller is here");
+
+
+
+	var myHand = angular.element(document.getElementById("my-hand")),
+		PCHand = angular.element(document.getElementById("PC-hand"));
+	myHand.on("click", function(e){
+		var myHandParent = myHand.parent();
+		var PCHandParent = PCHand.parent();
+		myHandParent.toggleClass("s12");
+		myHandParent.toggleClass("s6");
+		PCHandParent.slideToggle(0);
+		
+	});
+
 });
--- js/tmp_init.js
+++ js/tmp_init.js
@@ -2,16 +2,16 @@
   $(function(){
 
   
-	var myHand = $("#my-hand"),
-		PCHand = $("#PC-hand");
-		myHand.on("click", function(e){
-			var myHandParent = myHand.parent();
-			var PCHandParent = PCHand.parent();
-			myHandParent.toggleClass("s12");
-			myHandParent.toggleClass("s6");
-			PCHandParent.slideToggle(0);
+	// var myHand = $("#my-hand"),
+	// 	PCHand = $("#PC-hand");
+	// 	myHand.on("click", function(e){
+	// 		var myHandParent = myHand.parent();
+	// 		var PCHandParent = PCHand.parent();
+	// 		myHandParent.toggleClass("s12");
+	// 		myHandParent.toggleClass("s6");
+	// 		PCHandParent.slideToggle(0);
 		
-	});
+	// });
 
 
   }); // end of document ready
