fa0d40263280534f9dae670a31669e4b85a9972e
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-03-26 23:12:53 +0200

    -

--- js/rpsApp.js
+++ js/rpsApp.js
@@ -12,14 +12,29 @@ rpsApp.controller("rpsCtrl", function($scope){
 
 
 	var myHand = angular.element(document.getElementById("my-hand")),
-		PCHand = angular.element(document.getElementById("PC-hand"));
+		PCHand = angular.element(document.getElementById("PC-hand")),
+		scoreRow = angular.element(document.getElementById("scoreRow")),
+		stepConditionRow = angular.element(document.getElementById("stepConditionRow")),
+		againstPCRow = angular.element(document.getElementById("againstPCRow")),
+		rpsOptionsRow = angular.element(document.getElementById("rpsOptionsRow"));
 	myHand.on("click", function(e){
 		var myHandParent = myHand.parent();
 		var PCHandParent = PCHand.parent();
-		myHandParent.toggleClass("s12");
-		myHandParent.toggleClass("s6");
-		PCHandParent.slideToggle(0);
-		
-	});
+		// myHand.toggleClass("bigger-font");
 
+		scoreRow.slideToggle(0);
+		stepConditionRow.slideToggle(0);
+		againstPCRow.slideToggle(0);
+		rpsOptionsRow.slideToggle(0);
+	});
+});
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+rpsApp.directive("mainInfoCard", function () {
+    return function (scope, element, attrs) {
+         		var tmpBold = angular.element(document.createElement('b'));
+         		tmpBold.text("bbbooollldd");
+         		element.append(tmpBold);
+        }
 });
