f98ec4d7e9fff5388ed6eeb3fa413f9033a1e086
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-04-16 17:21:34 +0300

    test

--- js/rpsApp.js
+++ js/rpsApp.js
@@ -1,23 +1,28 @@
 var rpsApp = angular.module('rpsApp', ["ngRoute"]);
 
-rpsApp.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider){
-	// $locationProvider.html5Mode({
-	// 	enabled: true,
-	// 	requireBase: false
-	// });
-	$routeProvider.when('/gamespace', {
-		templateUrl:'views/gameSpace.html',
-		controller:'rpsCtrl'
-	});
-	$routeProvider.when('/userinfo', {
-		templateUrl:'views/userInfo.html',
-		controller:'rpsCtrl'
-	});
-	$routeProvider.when('/maincard', {
-		templateUrl:'views/mainCard.html',
-		controller:'rpsCtrl'
-	});
-	$routeProvider.otherwise({redirectTo: '/maincard'});//маршрут за замовчуванням
+rpsApp.config(["$routeProvider", "$locationProvider", "$httpProvider", function($routeProvider, $locationProvider, $httpProvider) {
+    // $locationProvider.html5Mode({
+    // 	enabled: true,
+    // 	requireBase: false
+    // });
+//$httpProvider.defaults.headers.post['Access-Control-Allow-Origin'] = 'http://127.0.0.1:1337/';
+//delete $httpProvider.defaults.headers.common['X-Requested-With'];
+$httpProvider.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+    $routeProvider.when('/gamespace', {
+        templateUrl: 'views/gameSpace.html',
+        controller: 'rpsCtrl'
+    });
+    $routeProvider.when('/userinfo', {
+        templateUrl: 'views/userInfo.html',
+        controller: 'rpsCtrl'
+    });
+    $routeProvider.when('/maincard', {
+        templateUrl: 'views/mainCard.html',
+        controller: 'rpsCtrl'
+    });
+    $routeProvider.otherwise({
+        redirectTo: '/maincard'
+    }); //маршрут за замовчуванням
 }]);
 
 // rpsApp.run(function($rootScope, $timeout, $templateCache) {
@@ -27,210 +32,256 @@ rpsApp.config(["$routeProvider", "$locationProvider", function($routeProvider, $
 //         }
 
 //     });
-rpsApp.controller("mainCardCtrl", ["$scope", "$timeout", "$location", function($scope, $timeout, $location){
+rpsApp.controller("mainCardCtrl", ["$scope", "$timeout", "$location", function($scope, $timeout, $location) {
 
 }]);
-rpsApp.controller("rpsCtrl", ["$scope", "$timeout", "$http", "$location", "$route", "$templateCache", function($scope, $timeout, $http, $location, $route, $templateCache){
+rpsApp.controller("rpsCtrl", ["$scope", "$timeout", "$http", "$location", "$route", "$templateCache", function($scope, $timeout, $http, $location, $route, $templateCache) {
+		//видалення кешування сторінок
+    // $scope.$on('$routeChangeStart', function(event, next, current) {
+    //     if (typeof(current) !== 'undefined') {
+    //         $templateCache.remove(next.templateUrl);
+    //         // console.log(next);
+    //         // console.log(current);
+    //     }
+    // });
 
-$scope.$on('$routeChangeStart',  function(event, next, current){
-       if (typeof(current) !== 'undefined'){
-            $templateCache.remove(next.templateUrl);
-            console.log(next);
-            console.log(current);
-        }
-});
-
-//тимчасовий масив статистики
-var arrOfStats = [];
-for (var i = 0; i < 3; i++) {
-	for (var j = 0; j < 3; j++) {
-		for (var k = 0; k < 3; k++) {
-			for (var l = 0; l < 3; l++) {
-				var tmpIndex = "" + i + j + k + l;
-				arrOfStats[tmpIndex] = { 0:randomInteger(0,1), 1:randomInteger(0,1), 2:randomInteger(0,1) };
-			};
-		};
-	};
-};
-
-//тимчасовий масив ходів
-var arrOfMoves = [];
-for (var i = 0; i < 20; i++) {
-	arrOfMoves.push("" + randomInteger(0,2) + randomInteger(0,2));
-};
-function randomInteger(min, max) {
-	var rand = min + Math.random() * (max - min)
-	rand = Math.round(rand);
-	return rand;
-};
-
-//звернення до статистики
-function userHandFromStats(currentSituation) {
-	var result = 0,
-	tmp = arrOfStats[currentSituation][result];
-	for (var i = 1; i < 3; i++){
-		if(arrOfStats[currentSituation][i] > tmp){
-			tmp = arrOfStats[currentSituation][i];
-			result = i;
-		};
-	};
-	return result;//повертає руку, яку найчастіше використовує гравець "за даних обставин"
-};
-
-// вибір ходу пк відносно ходу гравця зі статистики
-function choseMove(userHand){
-	switch(userHand){
-		case 0: {
-			return 1;
-		};
-		break;
-		case 1: {
-			return 2;
-		};
-		break;
-		case 2: {
-			return 0;
-		};
-		break;
-	};
-};
-
-//поточне значення руки гравця
-function myCurrentHand(){
-	var myMove = myHand.children()[0].classList;
-	if(myMove.contains("fa-hand-rock-o")){
-		return 0;
-	} else if (myMove.contains("fa-hand-paper-o")){
-		return 1;
-	} else if (myMove.contains("fa-hand-scissors-o")){
-		return 2;
-	};
-};
-
-//зміна руки пк в DOM-дереві
-function makePcMove(pcMove){
-	var el = $("#pcHand i");
-	switch(pcMove){
-		case 0: {
-			el.removeClass();
-			el.addClass("fa fa-diamond");
-		};
-		break;
-		case 1: {
-			el.removeClass();
-			el.addClass("fa fa-file-o");
-		};
-		break;
-		case 2: {
-			el.removeClass();
-			el.addClass("fa fa-scissors fa-flip-horizontal");
-		};
-		break;
-	};
-};
-
-//визначення результату ходу(порівняння руки пк та гравця)
-function compareMoves (pcMove) {
-	var myMove = myCurrentHand();
-	if (pcMove === myMove){
-		return 2;
-	} else if((myMove===0 && pcMove===2)||(myMove===2 && pcMove===1)||(myMove===1 && pcMove===0)){
-		return 0;
-	} else if((myMove===2 && pcMove===0)||(myMove===1 && pcMove===2)||(myMove===0 && pcMove===1)){
-		return 1;
-	};
-};
-
-//рухунок
-$scope.pcScore = 0;
-$scope.myScore = 0;
-$scope.drawScore = 0;
-$scope.resMessage;
-//опрацювання результату партії
-function resultProcessing (res) {
-	switch(res){
-		case 0: {
-			$scope.myScore++;
-			$scope.resMessage = "You won!!!";
-		};
-		break;
-		case 1: {
-			$scope.pcScore++;
-			$scope.resMessage = "You lost!!!";
-		};
-		break;
-			case 2: {// is it needed? :)
-	$scope.drawScore++;
-	$scope.resMessage = "Its draw!";
-};
-break;
-};
-$scope.$apply();
-console.log($scope.myScore, $scope.drawScore, $scope.pcScore);
-}
-
-//оновлення масиву статистики
-function refreshArrOfStats (currentSituation, currentMove) {
-	arrOfStats[currentSituation][currentMove]++;
-}
-
-//хід
-function move(){
-	var pcMove,
-	currentMove = myCurrentHand(),
-	lastMove = arrOfMoves.length - 1,
-	preLastMove = lastMove - 1,
-	currentSituation = "" + arrOfMoves[preLastMove] + arrOfMoves[lastMove],
-	userHandStats = userHandFromStats(currentSituation);
-
-	pcMove = choseMove(userHandStats);
-	makePcMove(pcMove);
-	currentMoveResult = compareMoves(pcMove);
+    var tmpObjStats = {};
+    
+     // $http({method:'GET', url:'http://127.0.0.1:1337', params: {'id':'id','up': false}}).
+     //         success(function(data) {
+     //             console.log(data);
+     //        });
+   // function statsFromServer(){
+   	// var conf = {headers: {origin: "http://127.0.0.1:1337"}};
+    	$http.post("http://127.0.0.1:1337", "", {
+    		headers: {
+    			 "Access-Control-Allow-Origin" : "*",
+            "Access-Control-Allow-Methods" : "GET,POST,PUT,DELETE,OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With",
+            'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
+    		}
+    	}).
+    		success(function(data, status, headers, config) {
+                // tmpObjStats=data.question;
+              console.log(data);
+            }).error(function(data, status, headers, config) {
+                console.log("код відповіді: " + status);
+                console.log(headers);
+            });
+   	//}
+    //}
 
 
-	resultProcessing(currentMoveResult);
+    //тимчасовий масив статистики
+    var arrOfStats = [];
+    for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+            for (var k = 0; k < 3; k++) {
+                for (var l = 0; l < 3; l++) {
+                    var tmpIndex = "" + i + j + k + l;
+                    arrOfStats[tmpIndex] = {
+                        0: randomInteger(0, 1),
+                        1: randomInteger(0, 1),
+                        2: randomInteger(0, 1)
+                    };
+                };
+            };
+        };
+    };
 
-		arrOfMoves.push("" + currentMove + currentMoveResult);//додаємо поточний хід в масив ходів
-		refreshArrOfStats(currentSituation, currentMove);
+    //тимчасовий масив ходів
+    var arrOfMoves = [];
+    for (var i = 0; i < 20; i++) {
+        arrOfMoves.push("" + randomInteger(0, 2) + randomInteger(0, 2));
+    };
 
-	};
-//+++++++++++++++++++++++++++++++++++++++++++++++
+    function randomInteger(min, max) {
+        var rand = min + Math.random() * (max - min)
+        rand = Math.round(rand);
+        return rand;
+    };
 
-var myHand = angular.element(document.getElementById("myHand")),
-PCHand = angular.element(document.getElementById("pcHand")),
-scoreRow = angular.element(document.getElementById("scoreRow")),
-stepConditionRow = angular.element(document.getElementById("stepConditionRow")),
-againstPCRow = angular.element(document.getElementById("againstPCRow")),
-rpsOptionsRow = angular.element(document.getElementById("rpsOptionsRow")),
-rpsOptions = angular.element(document.querySelectorAll(".rps-window-option"));
+    //звернення до статистики
+    function userHandFromStats(currentSituation) {
+        var result = 0,
+            tmp = arrOfStats[currentSituation][result];
+        for (var i = 1; i < 3; i++) {
+            if (arrOfStats[currentSituation][i] > tmp) {
+                tmp = arrOfStats[currentSituation][i];
+                result = i;
+            };
+        };
+        return result; //повертає руку, яку найчастіше використовує гравець "за даних обставин"
+    };
 
-//+++++++++++++++++++++++++++++++++++++++++++++++
+    // вибір ходу пк відносно ходу гравця зі статистики
+    function choseMove(userHand) {
+        switch (userHand) {
+            case 0:
+                {
+                    return 1;
+                };
+                break;
+            case 1:
+                {
+                    return 2;
+                };
+                break;
+            case 2:
+                {
+                    return 0;
+                };
+                break;
+        };
+    };
 
-myHand.on("click", function(e){
-	     var moveToChoseOption = $timeout(function(){
-		scoreRow.slideToggle(0);
-		stepConditionRow.slideToggle(0);
-		againstPCRow.slideToggle(0);
-		rpsOptionsRow.slideToggle(0);
-	}, 300);
+    //поточне значення руки гравця
+    function myCurrentHand() {
+        var myMove = myHand.children()[0].classList;
+        if (myMove.contains("fa-hand-rock-o")) {
+            return 0;
+        } else if (myMove.contains("fa-hand-paper-o")) {
+            return 1;
+        } else if (myMove.contains("fa-hand-scissors-o")) {
+            return 2;
+        };
+    };
 
-});
-rpsOptions.on("click", function(e){
-	 var choseOption = $timeout(function(){
-		scoreRow.slideToggle(0);
-		stepConditionRow.slideToggle(0);
-		againstPCRow.slideToggle(0);
-		rpsOptionsRow.slideToggle(0);
-		myHand.children()[0].remove();
-// 		if(!myHand.children()[0].remove()){
-// console.log("iiiiiieeeeeeeee");///////////////////
-// 		};
-var chosenOption = angular.element(e.currentTarget).children()[0];
-myHand.append(chosenOption.cloneNode());
-move();
-}, 300);
+    //зміна руки пк в DOM-дереві
+    function makePcMove(pcMove) {
+        var el = $("#pcHand i");
+        switch (pcMove) {
+            case 0:
+                {
+                    el.removeClass();
+                    el.addClass("fa fa-diamond");
+                };
+                break;
+            case 1:
+                {
+                    el.removeClass();
+                    el.addClass("fa fa-file-o");
+                };
+                break;
+            case 2:
+                {
+                    el.removeClass();
+                    el.addClass("fa fa-scissors fa-flip-horizontal");
+                };
+                break;
+        };
+    };
 
-});
+    //визначення результату ходу(порівняння руки пк та гравця)
+    function compareMoves(pcMove) {
+        var myMove = myCurrentHand();
+        if (pcMove === myMove) {
+            return 2;
+        } else if ((myMove === 0 && pcMove === 2) || (myMove === 2 && pcMove === 1) || (myMove === 1 && pcMove === 0)) {
+            return 0;
+        } else if ((myMove === 2 && pcMove === 0) || (myMove === 1 && pcMove === 2) || (myMove === 0 && pcMove === 1)) {
+            return 1;
+        };
+    };
+
+    //рухунок
+    $scope.pcScore = 0;
+    $scope.myScore = 0;
+    $scope.drawScore = 0;
+    $scope.resMessage;
+    //опрацювання результату партії
+    function resultProcessing(res) {
+        switch (res) {
+            case 0:
+                {
+                    $scope.myScore++;
+                    $scope.resMessage = "You won!!!";
+                };
+                break;
+            case 1:
+                {
+                    $scope.pcScore++;
+                    $scope.resMessage = "You lost!!!";
+                };
+                break;
+            case 2:
+                { // is it needed? :)
+                    $scope.drawScore++;
+                    $scope.resMessage = "Its draw!";
+                };
+                break;
+        };
+        $scope.$apply();
+        console.log($scope.myScore, $scope.drawScore, $scope.pcScore);
+    }
+
+    //оновлення масиву статистики
+    function refreshArrOfStats(currentSituation, currentMove) {
+        arrOfStats[currentSituation][currentMove]++;
+    }
+
+    //хід
+    function move() {
+        var pcMove,
+            currentMove = myCurrentHand(),
+            lastMove = arrOfMoves.length - 1,
+            preLastMove = lastMove - 1,
+            currentSituation = "" + arrOfMoves[preLastMove] + arrOfMoves[lastMove],
+            userHandStats = userHandFromStats(currentSituation);
+
+        pcMove = choseMove(userHandStats);
+        makePcMove(pcMove);
+        currentMoveResult = compareMoves(pcMove);
+
+
+        resultProcessing(currentMoveResult);
+
+        arrOfMoves.push("" + currentMove + currentMoveResult); //додаємо поточний хід в масив ходів
+        refreshArrOfStats(currentSituation, currentMove);
+
+        statsFromServer();
+        console.log(tmpObjStats);
+    };
+
+
+
+    //+++++++++++++++++++++++++++++++++++++++++++++++
+
+    var myHand = angular.element(document.getElementById("myHand")),
+        PCHand = angular.element(document.getElementById("pcHand")),
+        scoreRow = angular.element(document.getElementById("scoreRow")),
+        stepConditionRow = angular.element(document.getElementById("stepConditionRow")),
+        againstPCRow = angular.element(document.getElementById("againstPCRow")),
+        rpsOptionsRow = angular.element(document.getElementById("rpsOptionsRow")),
+        rpsOptions = angular.element(document.querySelectorAll(".rps-window-option"));
+
+    //+++++++++++++++++++++++++++++++++++++++++++++++
+
+    myHand.on("click", function(e) {     
+        var moveToChoseOption = $timeout(function() {
+            scoreRow.slideToggle(0);
+            stepConditionRow.slideToggle(0);
+            againstPCRow.slideToggle(0);
+            rpsOptionsRow.slideToggle(0);
+        }, 300);
+
+    });
+    rpsOptions.on("click", function(e) { 
+        var choseOption = $timeout(function() {
+            scoreRow.slideToggle(0);
+            stepConditionRow.slideToggle(0);
+            againstPCRow.slideToggle(0);
+            rpsOptionsRow.slideToggle(0);
+            myHand.children()[0].remove();
+            // 		if(!myHand.children()[0].remove()){
+            // console.log("iiiiiieeeeeeeee");///////////////////
+            // 		};
+            var chosenOption = angular.element(e.currentTarget).children()[0];
+            myHand.append(chosenOption.cloneNode());
+            move();
+        }, 300);
+
+    });
 }]);
 
 // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
--- server.js
+++ server.js
@@ -20,98 +20,122 @@
 // console.log(jsonArrOfStats);
 
 
-var objOfStats = require('./node_modules/rpsModule');
-objOfStats = JSON.stringify(objOfStats);
-// console.log(objOfStats.objStats);
+var objWithStats = require('./node_modules/rpsModule');
+var JSONObjOfStats = JSON.stringify(objWithStats.objStats);
+console.log(typeof JSONObjOfStats);
+
 
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 var path = require('path');
-var server = new http.Server(function(req, res){
-	console.log(req.method, req.url)
+var server = http.createServer();
+
+server.on('request', function(req, res){
+	// console.log(req.method, req.url);
+	
 	var urlParsed = url.parse(req.url, true);
-	console.log(urlParsed);
-	if (urlParsed.pathname == '/echo' && urlParsed.query.message){
+
+	//console.log(urlParsed);
+	// if (urlParsed.pathname == '/echo'){
     //res.end(objOfStats);//http://127.0.0.1:1337/echo?message=Hello
-    fs.readFile('./node_modules/rpsModule/jsonStats.json', function(err, info){
-    	if(err){
-    		console.error(err);
-    		res.statusCode = 500;
-    		res.end("error on server!");
-    		return;
-    	} else {
-    		res.end(info);
-    		console.log(res);
-    	};
-    });
-    fs.writeFile("file.tmp", objOfStats, function(err) {//створюємо файл і записуємо в нього рядок
-    	if (err) throw err;
+     // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', "*");
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+//console.log(req.headers, res._headers);
+    	res.end(JSONObjOfStats);
+    	console.log("JSONObjOfStats was sent");
+    	// console.log(res);
+    // fs.readFile('./node_modules/rpsModule/jsonStats.json', function(err, info){
+    // 	if(err){
+    // 		console.error(err);
+    // 		res.statusCode = 500;
+    // 		res.end("error on server!");
+    // 		return;
+    // 	} else {
+    // 		res.statusCode = 200;
+    // 		res.setHeader('Content-Type',  "; charset=utf-8");
+    // 		res.end(objOfStats);
+    // 		console.log("it's alive!!!!!!!");
+    // 	};
+    // });
+  //   fs.writeFile("file.tmp", objOfStats, function(err) {
+  //   	if (err) throw err;
 
 
-    });
-  } else{
-  	res.statusCode = 404;
-  	res.end("Page not found");
-  }
-   if (!checkAccess(req)) {
-        res.statusCode = 403;
-        res.end("Tell me the secret to access!");
-        return;
-    }
+  //   });
+  // } else{
+  // 	res.statusCode = 404;
+  // 	res.end("Page not found");
+  // }
+  //  if (!checkAccess(req)) {
+  //       res.statusCode = 403;
+  //       res.end("Tell me the secret to access!");
+  //       return;
+  //   }
 
-    sendFileSafe(url.parse(req.url).pathname, res);
+  //   sendFileSafe(url.parse(req.url).pathname, res);
+//}
 });
 server.listen(1337, '127.0.0.1');
 
-function checkAccess(req) {
-    return url.parse(req.url, true).query.secret == 'o_O';
-}
+// function checkAccess(req) {
+//     return url.parse(req.url, true).query.secret == 'o_O';
+// }
 
-function sendFileSafe(filePath, res) {//декодування url
+// function sendFileSafe(filePath, res) {//декодування url
 
-    try {
-        filePath = decodeURIComponent(filePath); // %D1%8F = я
-    } catch(e) {
-        res.statusCode = 400;
-        res.end("Bad Request");
-        return;
-    }
+//     try {
+//         filePath = decodeURIComponent(filePath); // %D1%8F = я
+//     } catch(e) {
+//         res.statusCode = 400;
+//         res.end("Wrong Request");
+//         return;
+//     }
 
-    if (~filePath.indexOf('\0')) {//перевірка розкодованого url на перший байт, якого не має бути
-        res.statusCode = 400;
-        res.end("Bad Request");
-        return;
-    }
+//     if (~filePath.indexOf('\0')) {//перевірка розкодованого url на перший байт, якого не має бути
+//         res.statusCode = 400;
+//         res.end("Wrong Request");
+//         return;
+//     }
 
-    //  /deep/nodejs.jpg ->  /Users/learn/node/path/public/deep/nodejs.jpg
-    filePath = path.normalize(path.join(ROOT, filePath));//отримати повний шлях до файлу на диску
+//     //  /deep/nodejs.jpg ->  /Users/learn/node/path/public/deep/nodejs.jpg
+//     filePath = path.normalize(path.join(ROOT, filePath));//отримати повний шлях до файлу на диску
 
-    if (filePath.indexOf(ROOT) != 0) {//перевірка на "префікс" файла - чи знаходиться файл у дозволеній дерикторії
-        res.statusCode = 404;
-        res.end("File not found");
-        return;
-    }
+//     if (filePath.indexOf(ROOT) != 0) {//перевірка на "префікс" файла - чи знаходиться файл у дозволеній дерикторії
+//         res.statusCode = 404;
+//         res.end("File not found");
+//         return;
+//     }
 
-    fs.stat(filePath, function(err, stats) {//перевіряємо що лежить по даному шляху
-        if (err || !stats.isFile()) {
-            res.statusCode = 404;
-            res.end("File not found");
-            return;
-        }
+//     fs.stat(filePath, function(err, stats) {//перевіряємо що лежить по даному шляху
+//         if (err || !stats.isFile()) {
+//             res.statusCode = 404;
+//             res.end("File not found");
+//             return;
+//         }
 
-        sendFile(filePath, res);//відсилаємо файл
-    });
-}
+//         sendFile(filePath, res);//відсилаємо файл
+//     });
+// }
 
-function sendFile(filePath, res) {
+// function sendFile(filePath, res) {
 
-    fs.readFile(filePath, function(err, content) {
-        if (err) throw err;
+//     fs.readFile(filePath, function(err, content) {
+//         if (err) throw err;
 
-        var mime = require('mime').lookup(filePath); // npm install mime
-        res.setHeader('Content-Type', mime + "; charset=utf-8"); // text/html image/jpeg
-        res.end(content);
-    });
+//         var mime = require('mime').lookup(filePath); // npm install mime
+//         res.setHeader('Content-Type', mime + "; charset=utf-8"); // text/html image/jpeg
+//         res.end(content);
+//     });
 
-}
+// }
