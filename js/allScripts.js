6ba9a98dfbf9d11e2d8db6a5d4f638ff34345381
Yaroslav Putria <yaroslav.putria@gmail.com>
2016-07-15 16:06:59 +0300

    beta

--- js/allScripts.js
+++ js/allScripts.js
@@ -1,16 +1,46 @@
-String.prototype.capitalizeFirstLetter = function() {
-	return this.charAt(0).toUpperCase() + this.slice(1);
+function capitalizeFirstLetter(str) {
+	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
-String.prototype.formatBirthYear = function() {
-	return this.split("BB").join(" BB");
+function formatBirthYear(str) {
+	return str.split("BB").join(" BB");
+}
+
+function loadingProcess() {
+	var loading = document.querySelector("#loading");
+	if (loading !== null) {
+		loading.hidden = !loading.hidden;
+	} else {
+		loading = document.createElement("div");
+		loading.style.cssText = "background-color: #000; width: 100%;	height: 100%;	position: absolute;	top: 0; z-index: 100";
+		loading.id = "loading";
+		document.body.appendChild(loading);
+	}
+}
+
+function checkNavButtons(id) {
+	var prevBtn = document.getElementById("prev-hero-btn"),
+		nextBtn = document.getElementById("next-hero-btn");
+	if (id === 1) {
+		prevBtn.disabled = true;
+	} else if (id === 88) {
+		nextBtn.disabled = true;
+	} else {
+		if (prevBtn.disabled) {prevBtn.disabled = !prevBtn.disabled};
+		if (nextBtn.disabled) {nextBtn.disabled = !nextBtn.disabled};
+	}
 }
 
 //'Access-Control-Request-Headers': 'X-Custom-Header',
-function getAjaxHero(id) {
-	var currentId = id,
+function makeHero(id) {
+	var currentId = id, // delete
 		currentHeroInfoUl,
 		currentHeroFilmsUl;
+
+	loadingProcess();
+
+	checkNavButtons(id);
+
 	return fetch(
 			"http://swapi.co/api/people/" + id + "/",
 			{
@@ -23,37 +53,59 @@ function getAjaxHero(id) {
 					'Connection': 'keep-alive'
 				}),
 			}
-		).then(function(response){
-			var contentType = response.headers.get("content-type");
-			if(contentType && contentType.indexOf("application/json") !== -1) {
-				return response.json().then(function(heroObj) {
-					currentHeroInfoUl = makeHeroInfoUl(heroObj);
-					return heroObj.films;
-				}).then(function(films) {
-					var filmPromises = films.map(film => getAjaxFilm(film));
-					return Promise.all(filmPromises);
-				}).then(function(allEpisodObjs){
-					allEpisodObjs.sort(function(a,b){
-						return a.episodeId > b.episodeId
-					});
-					var episodNames = allEpisodObjs.map(i => i.name);
-					currentHeroFilmsUl = makeFilmsInfoUl(episodNames);
-				}).then(function(){
-					//tmp
-					var forHero = document.querySelector(".wrap-hero-info");
-					forHero.appendChild(currentHeroInfoUl);
-					var forFilms = document.querySelector(".wrap-films");
-					forFilms.appendChild(currentHeroFilmsUl);
-					return currentId;
-				});
-			}	else {
-				console.log("There is no JSON!");
-			}
+		)
+		.then(function(response){//можна було всі функції з чейнінга описати окремо
+			if(response.ok) {
+				var contentType = response.headers.get("content-type");
+				if(contentType && contentType.indexOf("application/json") !== -1) {
+					return response.json();
+				}	else {
+					console.log("There is no JSON!");
+				};
+			} else {
+				throw new Error("There is no such hero");
+			};
+		})/*.catch(function(err) {
+			console.dir(err);
+		})*/
+		.then(function(heroObj) {
+			currentHeroInfoUl = makeHeroInfoUl(heroObj);
+			return heroObj.films;
+		})
+		.then(function(films) {
+			var filmPromises = films.map(film => getFilm(film));
+			return Promise.all(filmPromises);
+		})
+		.then(function(allEpisodObjs){
+			allEpisodObjs.sort(function(a,b){
+				return a.episodeId > b.episodeId
+			});
+			var episodNames = allEpisodObjs.map(i => i.name);
+			currentHeroFilmsUl = makeFilmsInfoUl(episodNames);
+		})
+		.then(function(){
+			//tmp
+			var forHero = document.querySelector(".wrap-hero-info");
+			while (forHero.firstChild) {
+				forHero.removeChild(forHero.firstChild);
+			};
+			forHero.appendChild(currentHeroInfoUl);
+			var forFilms = document.querySelector(".wrap-films");
+			while (forFilms.firstChild) {
+				forFilms.removeChild(forFilms.firstChild);
+			};
+			forFilms.appendChild(currentHeroFilmsUl);
+
+			loadingProcess();
+			
+			return currentId;
+		}).catch(function(err) {
+			loadingProcess();
+			console.log(err.message);
 		});
 }
 
-function getAjaxFilm(url) {
-	console.log(url);
+function getFilm(url) {
 	return fetch(
 			url,
 			{
@@ -69,16 +121,15 @@ function getAjaxFilm(url) {
 		).then(function(response){
 			var contentType = response.headers.get("content-type");
 			if(contentType && contentType.indexOf("application/json") !== -1) {
-				return response.json().then(function(filmObj) {
-					var fullFilmNameObj = {
-						name: "Episode " + filmObj.episode_id + ": " + filmObj.title,
-						episodeId: filmObj.episode_id
-					};
-					return fullFilmNameObj;
-				});
+				return response.json();
 			}	else {
 				console.log("There is no JSON!");
 			}
+		}).then(function(filmObj) {
+			return {
+				name: "Episode " + filmObj.episode_id + ": " + filmObj.title,
+				episodeId: filmObj.episode_id
+			};
 		});
 }
 
@@ -93,11 +144,11 @@ function makeHeroInfoUl(obj) {
 	ul.appendChild(makeLiItem("Name: " + obj.name));
 	ul.appendChild(makeLiItem("Height: " + obj.height + " m"));
 	ul.appendChild(makeLiItem("Mass: " + obj.mass + " Kg"));
-	ul.appendChild(makeLiItem("Hair Color: " + obj.hair_color.capitalizeFirstLetter()));
-	ul.appendChild(makeLiItem("Skin Color: " + obj.skin_color.capitalizeFirstLetter()));
-	ul.appendChild(makeLiItem("Eye Color: " + obj.eye_color.capitalizeFirstLetter()));
-	ul.appendChild(makeLiItem("Birth Year: " + obj.birth_year.formatBirthYear()));
-	ul.appendChild(makeLiItem("Gender: " + obj.gender.capitalizeFirstLetter()));
+	ul.appendChild(makeLiItem("Hair Color: " + capitalizeFirstLetter(obj.hair_color)));
+	ul.appendChild(makeLiItem("Skin Color: " + capitalizeFirstLetter(obj.skin_color)));
+	ul.appendChild(makeLiItem("Eye Color: " + capitalizeFirstLetter(obj.eye_color)));
+	ul.appendChild(makeLiItem("Birth Year: " + formatBirthYear(obj.birth_year)));
+	ul.appendChild(makeLiItem("Gender: " + capitalizeFirstLetter(obj.gender)));
 	return ul;
 }
 
@@ -109,6 +160,19 @@ function makeFilmsInfoUl(arrOfFilsm) {
 	return ul;
 }
 
-console.dir(getAjaxHero(1));
+var mainPromise = makeHero(1);
 
+var prevBtn = document.getElementById("prev-hero-btn"),
+	nextBtn = document.getElementById("next-hero-btn");
 
+nextBtn.addEventListener("mouseup", function(e) {
+	mainPromise.then(function(id) {
+		mainPromise = makeHero(id+1);
+	})
+});
+
+prevBtn.addEventListener("mouseup", function(e) {
+	mainPromise.then(function(id) {
+		mainPromise = makeHero(id-1);
+	})
+});
