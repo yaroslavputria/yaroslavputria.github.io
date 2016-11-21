86b2864a95dab92f2cc96d8d51251b1fcdaa228e
Yaroslav Putria <yaroslav.putria@synapse.com>
2016-11-21 15:11:04 +0200

    Revert "test"
    This reverts commit 528ffceb909ddd23ecb0a19481e46e1e2f044752.

---
+++ js/tmp_init.js
@@ -0,0 +1,36 @@
+// $("ul li").click(function(e) {
+
+//   /* Add the slider movement */
+
+//   // what tab was pressed
+//   var whatTab = $(this).index();
+
+//   // Work out how far the slider needs to go
+//   var howFar = 160 * whatTab;
+
+//   $(".slider").css({
+//     left: howFar + "px"
+//   });
+// });
+
+
+
+
+(function($){
+  $(function(){
+
+  
+	// var myHand = $("#my-hand"),
+	// 	PCHand = $("#PC-hand");
+	// 	myHand.on("click", function(e){
+	// 		var myHandParent = myHand.parent();
+	// 		var PCHandParent = PCHand.parent();
+	// 		myHandParent.toggleClass("s12");
+	// 		myHandParent.toggleClass("s6");
+	// 		PCHandParent.slideToggle(0);
+		
+	// });
+
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
